ac8ed558d9f409f3c50c61fa849fe828805fe5a0
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-12-03 15:07:37 +0000

    create function

--- flowershop/src/components/Product.js
+++ flowershop/src/components/Product.js
@@ -3,7 +3,22 @@ import { flowers } from './FlowerDB';
 import {useState} from 'react'
 
 export default function Product(props){
+  const [inputValue,setInputValue]=useState();
+  const[cart,setCart]=useState([]); 
+  
+  const addCart = (flowerName,flowerPrice, quantity) => {
     
+    const item = {
+        flowerName: flowerName,
+        flowerPrice:flowerPrice,
+        quantity: parseInt(quantity),
+    };
+
+    const updatedCart = [...cart, item];
+    setCart(updatedCart);
+    props.setCart(updatedCart); 
+};
+
     return(
         flowers.map(flower=>
         <div className="grid-item">
@@ -14,9 +29,9 @@ export default function Product(props){
                     <h5 class="card-title">{flower.name} Price:{flower.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity" onChange={(e)=>setInputValue(e.target.value)}/>
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={()=>addCart(flower.name,flower.price,inputValue)}>Add to Cart</button>
                 </div>
             </div>
         </div>
