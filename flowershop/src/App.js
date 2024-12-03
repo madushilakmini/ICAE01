b15c86094e38d6ec51345d1a8d96bcc5fdd34635
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-12-03 15:09:25 +0000

    fix App.js

--- flowershop/src/App.js
+++ flowershop/src/App.js
@@ -1,11 +1,11 @@
 import './assets/CSS/layout.css';
 import Products from './components/Products';
-import {flowers} from './components/FlowerDB'
+
 
 function App() {
   return (
     <div className="main-grid-container">
-      <Products flowers={flowers}/>
+      <Products/>
     </div>
   );
 }
